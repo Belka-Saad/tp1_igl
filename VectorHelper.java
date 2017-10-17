581b44406af4f0e95f2722b03ec9c143f29a294a
z0loy <32884147+z0loy@users.noreply.github.com>
2017-10-17 23:51:39 +0100

    Create new file
    Creating VectorHelper Class with addElemnt() Method and Afficher()

---
+++ VectorHelper.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class VectorHelper {
+	
+public ArrayList<Integer> Vecteur;
+
+	
+public VectorHelper(int size){
+	Vecteur=new ArrayList<Integer>(size);
+                             }
+public void addVectElemnt(int value){
+	  Vecteur.add(value);
+                                    }
+public void Afficher(){
+	System.out.println(Vecteur.toString());
+                      }
+                      
+}                      
